3121d98c3be534e0c8657dde9386a508b8451913
lehuyen130901 <lehuyen130901@gmail.com>
2023-03-08 14:50:24 +0700

    creat Login page

--- src/features/Login/Login.jsx
+++ src/features/Login/Login.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import Input from "./components/Input/Input";
 import './Login.scss';
 import userApi from "../../api/user";
+import headerLogo from '../../image/header-logo.png'
+import headerClose from '../../image/header-close.png'
+
+
 
 const Login = () => {
     const [username, setUserName] = useState('');
@@ -28,33 +32,81 @@ const Login = () => {
     }
 
     return (
-        <div className="login">
-            <div className="login-header">
-                <h3>Login</h3>
+        // <div className="login">
+        //     <div className="login-header">
+        //         <h3>Login</h3>
+        //     </div>
+        //     <form className="login-form">
+        //         <Input
+        //             name="Username"
+        //             id="username"
+        //             value={username}
+        //             placeholder="Username"
+        //             getInputValue={getUsername}
+        //         >
+        //         </Input>
+        //         <Input
+        //             type="password"
+        //             name="password"
+        //             id="password"
+        //             value={password}
+        //             placeholder="Password"
+        //             getInputValue={getPassword}
+        //         >
+        //         </Input>
+        //         <button className="login-form-btn" onClick={(e) => { handleLogin(e) }}>
+        //             Login
+        //         </button>
+        //     </form>
+        // </div>
+
+        <div>
+            <div className="header">
+                <img src={headerLogo} alt="logo" class="header-logo-img" />
+                <img src={headerClose} alt="close button" class="header-close-img" />
             </div>
-            <form className="login-form">
-                <Input
-                    name="Username"
-                    id="username"
-                    value={username}
-                    placeholder="Username"
-                    getInputValue={getUsername}
-                >
-                </Input>
-                <Input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    placeholder="Password"
-                    getInputValue={getPassword}
-                >
-                </Input>
-                <button className="login-form-btn" onClick={(e) => { handleLogin(e) }}>
-                    Login
-                </button>
-            </form>
+            <div className="login">
+                <form action="" className="login-form">
+                    <h2>Đăng nhập</h2>
+                    <div class="label-container">
+                        <label for="login-email" class="login-label">Email</label>
+                        <span class="login-span">
+                            Chưa có tài khoản?
+                            <span class="span-action"> Đăng ký</span>
+                        </span>
+                    </div>
+                    <Input
+                        type="email"
+                        id="login-email"
+                        name="Username"
+                        value={username}
+                        placeholder="Username"
+                        getInputValue={getUsername}>
+                    </Input>
+
+                    <div class="label-container">
+                        <label for="login-password" class="login-label">Mật khẩu</label>
+                        <span class="login-span span-action">
+                            Xem
+                        </span>
+                    </div>
+                    <Input
+                        type="password"
+                        id="login-password"
+                        value={password}
+                        placeholder="Password"
+                        getInputValue={getPassword}
+                    ></Input>
+
+                    <button class="login-form-btn" onClick={(e) => { handleLogin(e) }}>
+                        Đăng nhập
+                    </button>
+
+                    <a href="#" class="forgot-password">Quên mật khẩu?</a>
+                </form>
+            </div>
         </div>
+
     )
 }
 
