834bb0a5b4a9edc3bdf949bdef4329c75777cb38
lehuyen130901 <lehuyen130901@gmail.com>
2023-04-04 21:42:31 +0700

    Login & SignUp

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { parse, stringify } from 'qs'
 
-const host = 'http://2ba9-2001-ee0-41a1-3ae6-f19c-6b91-ff4-983.ngrok.io'
+const host = 'http://89f5-2001-ee0-41a1-cd3e-b4ed-346d-32ab-ffbb.ngrok.io'
 
 const axiosClient = axios.create({
     baseURL: host + '/api',
