1c761892083d7af9e41443d951eedf85ec960030
lehuyen130901 <lehuyen130901@gmail.com>
2023-04-29 17:45:26 +0700

    Approve Post UI

--- src/features/ApprovePost/ApprovePostItem/ApprovePostItem.jsx
+++ src/features/ApprovePost/ApprovePostItem/ApprovePostItem.jsx
@@ -1,16 +1,29 @@
 import styles from './ApprovePostItem.module.scss';
 import { host } from '../../../api/axiosClient';
+import { useEffect, useState } from 'react';
 
-const CreatePostItem = ({ image, title, date, id}) => {
+const CreatePostItem = ({ image, title, date, id, isChecked, idPost}) => {
+
+    const [agreement, setAgreement] = useState(false);
+    const [uId, setUid] = useState('');
+
     const handleChange = (e) => {
-        const {id, checked} = e.target;
+        isChecked(e.target.checked); 
+        idPost(id);   
     }
+    // console.log(agreement);
+    // console.log(uId);
+
     return (
         <div className={styles.container}>
             <div className={styles.img_container}>
                 <div className={styles.thumbnail_img}>
                     <img src={image} alt="thumbnail image" />
-                    <input type="checkbox" name={id} onChange={handleChange}/>
+                    <input
+                        type="checkbox"
+                        id={id}
+                        onChange={handleChange}
+                    />
                 </div>
             </div>
             <div>
